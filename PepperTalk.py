07cae229aecbdff483049ddfe5ae1c5287a8ee9e
Paiva Dias Luca tapaivad <luca.paivadias@stud.hslu.ch>
2018-03-05 18:44:41 +0100

    fixed PepperTalk.py

--- PepperTalk.py
+++ PepperTalk.py
@@ -4,7 +4,7 @@ app = qi.Application(["--qi-url=" + connection_url])
 app.start()
 session = app.session
 tts = session.service("ALTextToSpeech")
-tts.say("\\rspd=150\")
+tts.say("\\rspd=150\ ")
 
 motion = session.service("ALMotion")
 motion.moveTo(1.0, 1.0, 0.0)
